0ab5858f980356704a5a1215c8a6b4c4bb9a9351
literalnansensu <howtueatlife@gmail.com>
2025-04-24 14:57:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("Dudung dan Maman asal Citaman")
+st.title("NEON GENESIS EVANGELION")
 st.write(
-    "aku cinta wow spaghetti, indomie seblak, dan tori miso, aku cinta greenhouse effect dan tame impale"
+    "
+    FINALE : Take care of yourself."
 )
