d097b604dbe40a265c4d3842dd775ff9998a7a0e
literalnansensu <howtueatlife@gmail.com>
2025-04-24 19:11:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 st.title("NEON GENESIS EVANGELION")
-st.write("FINALE : Take care of yourself."
+st.write(
+  "FINALE : Take care of yourself."
 )
-
-
-st.image("views/views/54d12abd-0cf9-4653-8f5f-6027cd197aeb.jpeg")
+st.image("views/54d12abd-0cf9-4653-8f5f-6027cd197aeb.jpeg")
